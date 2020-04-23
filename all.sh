3e30e6cbdc1d6193fbe38b987363b02eafa39e11
Lassi Kortela <lassi@lassi.io>
2020-04-23 10:30:18 +0300

    Turn off ironscheme until we know how to fix it

--- all.sh
+++ all.sh
@@ -2,7 +2,7 @@
 set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
-for scheme in bigloo chezscheme chibi chicken cyclone gauche guile ikarus ironscheme kawa larceny mit mosh racket vicare; do
+for scheme in bigloo chezscheme chibi chicken cyclone gauche guile ikarus kawa larceny mit mosh racket vicare; do
     echo "===== $scheme ====="
     ./"$scheme.sh"
     docker system prune -f
