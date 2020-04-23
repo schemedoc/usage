ad61eb5af1b4316b29ccf7e6a4f348aff7cb93b1
Lassi Kortela <lassi@lassi.io>
2020-04-23 11:14:46 +0300

    Break long line

--- all.sh
+++ all.sh
@@ -2,8 +2,10 @@
 set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
-for scheme in bigloo chezscheme chibi chicken cyclone gauche guile ikarus kawa larceny mit-scheme mosh racket vicare
-do
+for scheme in \
+    bigloo chezscheme chibi chicken cyclone \
+    gauche guile ikarus kawa larceny \
+    mit-scheme mosh racket vicare; do
     echo "===== $scheme ====="
     ./"$scheme.sh"
     docker system prune -f
