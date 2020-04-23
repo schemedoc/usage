3eda721e209883c24729a84b6dc1fe5de8003376
Lassi Kortela <lassi@lassi.io>
2020-04-23 10:36:35 +0300

    Fix all.sh

--- all.sh
+++ all.sh
@@ -2,9 +2,11 @@
 set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
-for scheme in bigloo chezscheme chibi chicken cyclone gauche guile ikarus kawa larceny mit-scheme mosh racket vicare; do
+for scheme in bigloo chezscheme chibi chicken cyclone gauche guile ikarus kawa larceny mit-scheme mosh racket vicare
+do
     echo "===== $scheme ====="
     ./"$scheme.sh"
     docker system prune -f
-    docker rmi "schemers/$scheme" "schemers/$scheme:head" || true
+    docker rmi -f "schemers/$scheme"
+    docker rmi -f "schemers/$scheme:head"
 done
