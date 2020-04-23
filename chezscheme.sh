b2fddb5c118130908666a6ad2e16e42c19ee7602
Lassi Kortela <lassi@lassi.io>
2020-04-23 11:59:08 +0300

    Fix output to stderr
    Some Schemes apparently act weird when run from Docker without a
    terminal, writing the usage message to stderr even though they
    normally write it to stdout. Fix by redirecting stderr to stdout.

--- chezscheme.sh
+++ chezscheme.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eux
-docker run schemers/chezscheme scheme --help >chezscheme.text
+docker run schemers/chezscheme sh -c 'scheme --help 2>&1' >chezscheme.text
--- mosh.sh
+++ mosh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eux
-docker run schemers/mosh:head sh -c 'mosh -h || true' >mosh.text
+docker run schemers/mosh:head sh -c 'mosh -h 2>&1 || true' >mosh.text
