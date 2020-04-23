9dc9cc42f2d68d49881f554699a44ffaf911e3e1
Lassi Kortela <lassi@lassi.io>
2020-04-23 15:48:06 +0300

    Add Gambit runtime options

--- gambit.sh
+++ gambit.sh
@@ -2,3 +2,5 @@
 set -eux
 docker run schemers/gambit:head gsc -h >gambit-gsc.text
 docker run schemers/gambit:head gsi -h >gambit-gsi.text
+docker run schemers/gambit:head sh -c 'gsi -:help 2>&1 || true' \
+    >gambit-runtime.text
