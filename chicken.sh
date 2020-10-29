2ee3a417eff0db4f25606d919b7927a9a2bb65b1
Lassi Kortela <lassi@lassi.io>
2020-10-29 13:34:22 +0200

    Add Chicken runtime options

--- chicken.sh
+++ chicken.sh
@@ -2,3 +2,4 @@
 set -eux
 docker run schemers/chicken:head csc --help >chicken-csc.text
 docker run schemers/chicken:head csi --help >chicken-csi.text
+docker run schemers/chicken:head sh -c 'csi -:? 2>&1' >chicken-runtime.text
